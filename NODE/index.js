ddca97625c004494dca1860b8d1c669de64d3fce
DhairyaSinghHariom <sdhairya192@gmail.com>
2026-03-19 11:31:08 +0530

    Enhance NODE/index.js with asynchronous timeouts
    Added multiple setTimeout calls for asynchronous logging.

--- NODE/index.js
+++ NODE/index.js
@@ -19,3 +19,34 @@ fs.readFile('Data.txt', 'utf8', (err, data) => {
     }
     console.log(data);
 });
+// Synchronous 
+
+console.log("start");
+setTimeout(() => {
+    console.log("This is a timeout");
+}, 2000);
+;
+
+
+setTimeout(() => {
+    console.log(1 + "This is a timeout");
+}, 3000);
+
+setTimeout(() => {
+    console.log(2 +"This is a timeout");
+}, 4000);
+
+setTimeout(() => {
+    console.log(3 + "This is a timeout");
+}, 5000);
+
+setTimeout(() => {
+    console.log( 4 + "This is a timeout");
+}, 1000);
+console.log("end");
+
+
+// Asynchronous 
+//   Method call
+// Call backs , Promises , Async /Await
+
