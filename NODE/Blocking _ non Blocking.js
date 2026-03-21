e8f079e74d4c76a01dc1bf4692ab8f95abfc3bcd
DhairyaSinghHariom <sdhairya192@gmail.com>
2026-03-21 20:24:11 +0530

    Update Blocking _ non Blocking.js

--- NODE/Blocking _ non Blocking.js
+++ NODE/Blocking _ non Blocking.js
@@ -1,18 +1,29 @@
+const { log } = require("console");
 const fs = require("fs");
 
 // Blocking 
 
-const result = fs.readFileSync("contacts.txt", "utf-8");
-console.log(result);
+// console.log("2");
+// const result = fs.readFileSync("contacts.txt", "utf-8");
+// console.log(result);
+
+
+// console.log("7");
 
 console.log("2");
-
 // Non-blocking
 
-const Result = fs.readFileSync("contacts.txt", "utf-8");
-console.log(Result);
+const Result = fs.readFile("contacts.txt", "utf-8", (err, Result) => {
+  console.log(Result);
+  
+});
 
-console.log("2");
+
 console.log("3");
 console.log("4");
+console.log("5");
+console.log("6");
+
+
+
 
