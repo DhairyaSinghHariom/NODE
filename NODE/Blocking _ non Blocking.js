7980d0d5667349bb529af85e5296b969f38fa167
DhairyaSinghHariom <sdhairya192@gmail.com>
2026-03-21 20:59:16 +0530

    Update Blocking _ non Blocking.js

--- NODE/Blocking _ non Blocking.js
+++ NODE/Blocking _ non Blocking.js
@@ -1,17 +1,20 @@
 const { log } = require("console");
 const fs = require("fs");
+const os = require("os");
+console.log(os.cpus().length);
+
 
 // Blocking 
 
-// console.log("2");
-// const result = fs.readFileSync("contacts.txt", "utf-8");
-// console.log(result);
+console.log("2");
+const result = fs.readFileSync("contacts.txt", "utf-8");
+console.log(result);
 
 
-// console.log("7");
+console.log("7");
 
 console.log("2");
-// Non-blocking
+// // Non-blocking
 
 const Result = fs.readFile("contacts.txt", "utf-8", (err, Result) => {
   console.log(Result);
@@ -25,5 +28,6 @@ console.log("5");
 console.log("6");
 
 
-
+// Default Thead Pool Size =4 
+// Max ? - 8 core cpu - 8
 
